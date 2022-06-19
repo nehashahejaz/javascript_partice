f1911bdf27b37a43f8ea0bf6a9105b5761c8640c
neha shah <66621555+nehashahejaz@users.noreply.github.com>
2022-06-19 09:17:28 +0500

    Update myapp.js
    done

--- myapp.js
+++ myapp.js
@@ -1,4 +1,4 @@
-                // chapter 38 to 42
+                              // chapter 38 to 42
 // qno1Write a custom function power ( a, b ), to calculate the value of
 // a raised to b.
 
@@ -42,16 +42,52 @@
 // where, S = ( a + b + c ) / 
 
 
-var a= +prompt("enter side");
+// var a= +prompt("enter side");
 
- var b = +prompt("enter 2nd side");
- var c = +prompt("enter 3rd side");
- var areaa= tria(a,b,c);
- document.write("area  of triangle is "+ areaa);
- function tria(a,b,c){
-var s = (a + b + c)/2;
-var area =Math.sqrt(s*((s-a)*(s-b)*(s-c)));
-return area;
+//  var b = +prompt("enter 2nd side");
+//  var c = +prompt("enter 3rd side");
+//  var areaa= tria(a,b,c);
+//  document.write("area  of triangle is "+ areaa);
+//  function tria(a,b,c){
+// var s = (a + b + c)/2;
+// var area =Math.sqrt(s*((s-a)*(s-b)*(s-c)));
+// return area;
 
- }
+// //  }
+// chapter 42 to 48
+
+
+// Task # 1
+// function showAlert() 
+// {
+//     alert("You clicked Me!");
+// }
+
+// function deleteRow(id)
+// {
+//     document.getElementById(id).remove();
+// }
+
+// function changeImage()
+// {
+//     document.getElementById("myImage").src = 'images/InfinixNote7-b.jpg';
+// }
+
+// function restoreImage() {
+//     document.getElementById("myImage").src = 'images/SamsungGalaxyA31-b.jpg';
+// }
+
+
+// var count = parseInt(document.getElementById('counter').innerHTML);
+
+// function increaseCount() {
+//     count++;
+//     document.getElementById("counter").innerHTML = count;
+// }
+
+// function decreaseCount() {
+//     count--;
+//     document.getElementById("counter").innerHTML = count;
+// }
+
 
