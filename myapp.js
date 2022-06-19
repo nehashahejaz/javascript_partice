023c402fcef481ca9b75c3006a3cf06f0218fef0
neha shah <66621555+nehashahejaz@users.noreply.github.com>
2022-06-19 09:13:47 +0500

    Add files via upload
    dne

---
+++ myapp.js
@@ -0,0 +1,57 @@
+                // chapter 38 to 42
+// qno1Write a custom function power ( a, b ), to calculate the value of
+// a raised to b.
+
+// var a = +prompt("enter base numbeer");
+
+// var b = +prompt("enter power");
+// var res=cust(a,b);
+// document.write("the power"+b+" of"+a+"is"+res);
+
+// function cust(a,b){
+//     var power= Math.pow(a, b)
+//     return power;
+
+// }
+// qn2:Any year is entered through the keyboard. Write a function to
+// determine whether the year is a leap year or not.
+
+
+
+// function check_leapyear(){
+
+//     var year = +prompt("enter year");
+
+ 
+//     if( (0 == year % 4) && (0 != year % 100) || (0 == year % 400) )
+//     {
+//         alert(year+" is a leap year");  
+//     }
+//     else
+//     {
+//         alert(year+" is not a leap year");  
+//     }
+// }
+
+// check_leapyear();
+
+
+// qno3:If the lengths of the sides of a triangle are denoted by a, b, and
+// c, then area of triangle is given by
+// area = S(S − a)(S − b)(S − c)
+// where, S = ( a + b + c ) / 
+
+
+var a= +prompt("enter side");
+
+ var b = +prompt("enter 2nd side");
+ var c = +prompt("enter 3rd side");
+ var areaa= tria(a,b,c);
+ document.write("area  of triangle is "+ areaa);
+ function tria(a,b,c){
+var s = (a + b + c)/2;
+var area =Math.sqrt(s*((s-a)*(s-b)*(s-c)));
+return area;
+
+ }
+
